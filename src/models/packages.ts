c8a80dcde11082b8d85b08551a0f483e91939462
Rex Pan <napxer@gmail.com>
2022-07-19 20:07:20 +0700

    Update models/packages

--- src/models/packages.ts
+++ src/models/packages.ts
@@ -1,5 +1,7 @@
 
-export const packages = [
+export interface IPackage { id:string, packageName:string, addRatio: number }
+
+export const packages: IPackage[] = [
     { id:"sd", packageName:"Standard"  , addRatio:0, },
     { id:"sf", packageName:"Safe"      , addRatio:.5 },
     { id:"ss", packageName:"Super Safe", addRatio:.75},
